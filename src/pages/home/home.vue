af2d6c786f32b0e7a43e5040a9d8c3a57e7d6a00
Your Name <you@example.com>
2018-07-30 10:12:24 +0800

    a

---
+++ src/pages/home/home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        我是首页
+    </div>
+</template>
+
+<script>
+import {get} from './../../util.js'
+import {formatTime} from './../../utils'
+export default {
+    async created () {
+        const res = await get("/weapp/demo")
+        console.log("demo=>",res)
+        console.log("data=>",Date())
+        console.log("data=>", formatTime(new Date(Date.now()) ))
+    }
+}
+</script>
+<style>
+
+</style>
